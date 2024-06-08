200b14ce72e99cecfe3b9c65b7ee0d48409da867
Til33n <126579167+Til33n@users.noreply.github.com>
2024-06-08 15:49:00 +0200

    Update database_execute.py

--- database_execute.py
+++ database_execute.py
@@ -1,196 +1,47 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+import os
+import webbrowser
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+import requests
+from flask import Flask, render_template, request, jsonify
+import sys
+
+import smtplib, ssl
+from email.message import EmailMessage
 import sqlite3
+from database import show_all, add_one, delete_one, add_many, data_lookup    #SQLite 3
+import database
 
-conn = sqlite3.connect("data.db")
+#USED FOR SHOWING CURRENT DATA IN DATABASE AND GENERAL DEBUGGING 
 
-#creates a cursor
-c = conn.cursor()
-
-#creates a table                             # AFTER CREATION OF DATABASE, WE COMMENT THIS PROCESS OUT
-#c.execute("""CREATE TABLE data
-#(
-#    source_email text,
-#    verification_code text,
-#    destination_email text,
-#    time_of_post text,
-#    message text,
-#    input_file text  
-#
-#)""")
-
-#   DATATYPES
-#NULL - it doesnt exit
-#INTEGER - whole nubmer
-#REAL - decimel
-#TEXT - text/string
-#BLOB - image, mp3,...
-
-multiple_data = [   
-    ('11','12','13','14'),   # ?,?,?    TUPLE PLACEHOLDER WITH ?
-    ('21','22','23','24'),   # ?,?,?
-    ('31','32','33','34'),
-    ('41','42','43','44'),   # ?,?,?    TUPLE PLACEHOLDER WITH ?
-    ('51','52','53','54'),   # ?,?,?
-    ('61','62','63','64'),   # ?,?,?
-      
+test_data = [   
+    ('11','12','13','14','24'),   # ?,?,?    TUPLE PLACEHOLDER WITH ?
+    ('21','22','23','24','24'),   # ?,?,?
+    ('31','32','33','34','24'),
+    ('31','32','33','34','24'),
+    ('41','42','43','44','24'),   # ?,?,?    TUPLE PLACEHOLDER WITH ?
+    ('51','52','53','54','24'),   # ?,?,?
+    ('61','62','63','64','24'),   # ?,?,? 
       ]
 
-#c.execute ( "INSERT INTO random_data VALUES ('somebodyfells@gmail.com','forlife2004@gmail.com','current_date','blob')")
+dummy_entry = [('X','X','X','X','X'), ]  
+test_entry = "Picture.jpg"
 
-#c.executemany( "INSERT INTO random_data VALUES (?,?,?,?)", multiple_data )
-
-# Query the Database
-
-#c.execute("SELECT rowid, * FROM random_data")
-#c.execute("SELECT rowid, * FROM random_data")
-#c.execute("SELECT rowid, * FROM random_data WHERE time_of_post LIKE 'cu%' ")
-#c.execute("SELECT rowid, * FROM random_data WHERE time_of_post = '33' ")
-
-
-#print(c.fetchone()  )                                   #its a tuple, 0 means first item of first row 
-#print(c.fetchmany(2) )                                     #2 is number of fetched rows 
-#print(c.fetchall())                                       #returns as a python list
-
-#items = c.fetchall()
-#print("Email_1 " +  "\t\t Email_2" )
-#print("--------  " +  "\t\t--------")
-#for item in items:
-#    print(item[0] + " " + item [1] + "\t" + item[2] + " " + item[3]) 
-
-#c.execute("""UPDATE random_data SET time_of_post = "changed" WHERE rowid = 5""")
-#c.execute("DELETE from random_data WHERE rowid = 4")
-#c.execute("SELECT rowid, * FROM random_data WHERE time_of_post LIKE '3%' AND rowid rowid = 3 ")
-#c.execute("SELECT rowid, * FROM random_data WHERE time_of_post LIKE '3%' OR rowid rowid = 3 ")
-#c.execute("DROP TABLE random_data")  #deletes entire table
-
-#show all data in datatable
-def show_all():  
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("SELECT rowid, * FROM data")
-    items = c.fetchall()
-    for item in items:
-        print(item)
-    conn.commit()
-    conn.close()    
+#print(database.get_file())
+#database.add_one_last(test_entry)
+#database.add_many(dummy_entry)
+#database.delete_all()
+#database.delete_last_input()
+#print(type(database.get_file()))
+#input_data = [('11','12','13','14','24')]
+#database.add_many(input_data)
+database.show_all()     
 
 
-
-#adds new record to table
-#def add_one(source_email,destination_email,time_of_post,blob):                                 #email_1,email_2,time,blob
-def add_one(source_email,code,destination_email,time_of_post,blob):                                 #email_1,email_2,time,blob
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("INSERT INTO data VALUES (?,?,?,?,?,?)", ( 'source_email','code','destination_email','time_of_post','message','blob'))
-    conn.commit()
-    conn.close()
-
-
-
-#deletes a single given row of data
-def delete_one(id):
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("DELETE from data WHERE rowid = (?)", id)
-    conn.commit()
-    conn.close()
-
-
-#adds multiple data at a time
-def add_many(list):
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.executemany("INSERT INTO data VALUES (?,?,?,?,?,?)", (list))
-    conn.commit()
-    conn.close()
-
-
-
-#look up where is item
-def data_lookup(data):
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("SELECT rowid,  * from data WHERE time_of_post = (?)", (data,))
-    items = c.fetchall()
-    for item in items:
-        print(item)
-    conn.commit()
-    conn.close()
-
-
-
-def delete_all():
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("DELETE from data")
-    conn.commit()
-    conn.close()
-
-def get_last():
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("SELECT * FROM data ORDER BY rowid DESC LIMIT 1;")
-    items = c.fetchall()
-    for item in items:
-        print(item)
-    conn.commit()
-    conn.close()
-
-def get_code():
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("SELECT * FROM data ORDER BY rowid DESC LIMIT 1;")    
-    code=c.fetchone()[1]
-    return code
-
-def delete_last_input():
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("DELETE FROM data WHERE rowid = (SELECT MAX(rowid) FROM data);")
-    conn.commit()
-    conn.close()
-
-def get_recepients_email():
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("SELECT * FROM data ORDER BY rowid DESC LIMIT 1;")    
-    recepients_email=c.fetchone()[2]
-    return recepients_email
-
-def get_file():
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("SELECT * FROM data ORDER BY rowid DESC LIMIT 1;")    
-    file=c.fetchone()[4]
-    return file
-
-def get_last_returned():
-    conn = sqlite3.connect("data.db") 
-    c = conn.cursor()
-    c.execute("SELECT * FROM data WHERE rowid = (SELECT MAX(rowid) FROM data);")
-    zero = c.fetchone()[0]
-    c.execute("SELECT * FROM data WHERE rowid = (SELECT MAX(rowid) FROM data);")
-    one =  c.fetchone()[1]
-    c.execute("SELECT * FROM data WHERE rowid = (SELECT MAX(rowid) FROM data);")
-    two =  c.fetchone()[2]
-    c.execute("SELECT * FROM data WHERE rowid = (SELECT MAX(rowid) FROM data);")
-    tree=  c.fetchone()[3]
-    c.execute("SELECT * FROM data WHERE rowid = (SELECT MAX(rowid) FROM data);")
-    four=  c.fetchone()[4]
-    conn.commit()
-    conn.close()
-    return zero, one, two,tree, four
-
-#def add_one_last(file_path):
-#    conn = sqlite3.connect("data.db") 
-#    c = conn.cursor()
-#    c.execute("INSERT INTO data VALUES (?,?,?,?,?)", (list))
-#    c.fetchone()[4] = file_path
-#    conn.commit()
-#    conn.close()
-
-# Commit comand
-conn.commit()
-
-# Close connection
-conn.close()
 
